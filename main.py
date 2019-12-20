091b1f084eebb978a44f5c70ef3044ba7c8074fc
Junha Park <jpark3@scu.edu>
2019-12-20 15:18:09 -0800

    Scheduling algorithm translated from C++ to Python
    First translation of algorithm to Python
    
    main.py no longer runs c++ algorithm, python algorithm needs debugging
    
    Python algorithm is now working, needs touchups now
    
    prereqs key returns none if no prereqs, c++ files also removed
    
    Debugging mode added to main.py
    
    Branch is ready to be rebased and merged

--- main.py
+++ main.py
@@ -3,27 +3,8 @@ import sys
 import json
 import random
 import pymysql
-import subprocess
 
 app = Flask(__name__)
-
-# Paths to algorithm files
-algo = 'FourYearPlanAlgorithm'
-algoClass = algo + '/scuClass.cpp'
-algoHashMap = algo + '/HashMap.cpp'
-algoPlan = algo + '/FourYearPlan.cpp'
-algoStudent = algo + '/Student.cpp'
-algoDriver = algo + '/driver.cpp'
-
-# Path to input file for algorithm
-classObjectsFile = 'AlgorithmInput.txt'
-
-# Path to four-year JSON file
-fourYearPlanJson = 'FourYearPlan.json'
-
-# SQLAPI++ files
-sqlApiInclude = algo + '/SQLAPI/include'
-sqlApiLib = algo + '/SQLAPI/lib'
 
 # Paths to JSON files containing survey questions and options
 surveysPath = 'setup/surveyQuestionJsons'
@@ -31,6 +12,166 @@ csciEmphasesJson = surveysPath + '/csciEmphases.json'
 # prefersMajorClassesOnlyJson = surveysPath + '/prefersMajorClassesOnly.json'
 csciMajorReqsJson = surveysPath + '/csciMajorReqs.json'
 coreReqsJson = surveysPath + '/coreReqs.json'
+
+DEBUG_MODE = False
+
+if any(arg in ['-d', '--debug'] for arg in sys.argv):
+    print("Debug flag set to True")
+    DEBUG_MODE = True
+
+class scuClass:
+    def __init__(self, cID = "", name = "", sub = "", quart = "", creds = 0):
+        self.classInfo = {'classID': cID, 'fullName': name, 'subject': sub, 'quarters': quart, 'credits': creds}
+        self.preReqs = []
+
+    def pushPreReq(self, cID):
+        if DEBUG_MODE is True: print("scuClass.pushPreReq(): Appending " + cID + " as prereq to scuClass object")
+        self.preReqs.append(cID)
+
+    def getID(self):
+        return self.classInfo['classID']
+
+    def getName(self):
+        return self.classInfo['fullName']
+
+    def getSubject(self):
+        return self.classInfo['subject']
+
+    def getQuarters(self):
+        return self.classInfo['quarters']
+
+    def getCredits(self):
+        return self.classInfo['credits']
+
+    def getPrereqs(self):
+        return self.preReqs
+
+    def printDetails(self):
+        print("Class ID:", self.getID())
+        print("Name:", self.getName())
+        print("Subject:", self.getSubject())
+        print("Credits:", self.getCredits())
+        offered = "Offered during "
+        quartersMap = {'F': 'Fall ', 'W': 'Winter ', 'S': 'Spring ', 'E': 'Even school years (2020-21)', 'O': 'Odd school years (2019-2020)'}
+        for key, value in quartersMap.items():
+            if key in self.classInfo['quarters']:
+                offered += value
+        if all(key not in self.classInfo['quarters'] for key in ['E', 'O']):
+            offered += "every year"
+        print(offered)
+        print("Prereqs:", self.getPrereqs())
+
+    # What is q? How does this logic work?
+    def available(self, q, year):
+        quartersOffered = self.classInfo['quarters']
+        if (year % 2 == 0) and ('O' not in quartersOffered):
+            if quartersOffered.find(q[0]) is not -1:
+                return True
+        elif (year % 2 == 1) and ('E' not in quartersOffered):
+            if quartersOffered.find(q[0]) is not -1:
+                return True
+        return False
+
+class FourYearPlan:
+    # All constructor arguments are hash maps except for creditsAlreadyDone
+    def __init__(self, inputMajor, inputCore, creditsAlreadyDone):
+        self.metadata = {'major': inputMajor, 'core': inputCore, 'doneClasses': [], 'credits': creditsAlreadyDone}
+        self.quarters = ["F","W","S","F","W","S","F","W","S","F","W","S"]
+
+    def completeClass(self, cID):
+        maps = ['major', 'core']
+        for key in maps:
+            if cID in self.metadata[key]:
+                self.metadata['doneClasses'].append(cID)
+                self.metadata['credits'] += self.metadata[key][cID].getCredits()
+
+    def getClass(self, cID): # scuClass return value
+        maps = ['major', 'core']
+        for key in maps:
+            if cID in self.metadata[key]:
+                return self.metadata[key][cID]
+
+    # not sure if logic is correct, especially any condition
+    def feasible(self, cID, quarter, year):
+        if DEBUG_MODE is True: print("feasible(), cID:", cID)
+        if DEBUG_MODE is True: print("feasible(), quarter:", quarter)
+        if DEBUG_MODE is True: print("feasible(), year:", year)
+        if DEBUG_MODE is True: print("feasible(), self.metadata['doneClasses']:", self.metadata['doneClasses'])
+        if DEBUG_MODE is True: print("feasible(), self.getClass(cID).getPrereqs():", self.getClass(cID).getPrereqs())
+        if cID in self.metadata['doneClasses']:
+            if DEBUG_MODE is True: print("feasible() return False because class is done")
+            return False
+        if any(prereq not in self.metadata['doneClasses'] for prereq in self.getClass(cID).getPrereqs()):
+            if DEBUG_MODE is True: print("feasible() return False because prereqs not met")
+            return False
+        if DEBUG_MODE is True: print("feasible() return", self.getClass(cID).available(quarter, year))
+        return self.getClass(cID).available(quarter, year)
+
+    def planComplete(self):
+        maps = ['major', 'core']
+        if self.metadata['credits'] < 175:
+            if DEBUG_MODE is True: print("planComplete() return False because credits under 175")
+            return False
+        for key in maps:
+            for cID in self.metadata[key]:
+                if cID not in self.metadata['doneClasses']:
+                    if DEBUG_MODE is True: print("planComplete() return False because", cID, "not yet taken")
+                    return False
+        if DEBUG_MODE is True: print("planComplete() return True")
+        return True
+
+    def buildPlan(self, year):
+        plan = []
+        currentYear = -1
+        quarter = -1
+        maxClassCount = 4
+        terms = ['Fall', 'Winter', 'Spring']
+        maps = ['major', 'core']
+        while not self.planComplete():
+            quarter += 1
+            if quarter % 3 == 0:
+                quarter = 0
+                currentYear += 1
+                academicYear = str(year) + '-' + str(year + 1)
+                if DEBUG_MODE is True: print("buildPlan(), currentYear:", currentYear)
+                if DEBUG_MODE is True: print("buildPlan(), quarter:", quarter)
+                if DEBUG_MODE is True: print("buildPlan(), academicYear:", academicYear)
+                plan.append({'year': academicYear, 'yearSchedule': []})
+                if DEBUG_MODE is True: print("Current plan:", plan)
+            if quarter % 3 == 1:
+                year += 1
+            plan[currentYear]['yearSchedule'].append({'quarter': terms[quarter], 'classes': []})
+            classCount = 0
+            balanceCount = 0
+            for key in maps:
+                if key == 'core':
+                    balanceCount = 0
+                for cID in self.metadata[key]:
+                    if self.feasible(cID, terms[quarter][0], year) and (classCount < maxClassCount):
+                        if DEBUG_MODE is True: print("buildPlan(), appending", cID, "to plan")
+                        prereqs = self.metadata[key][cID].getPrereqs()
+                        if not prereqs:
+                            prereqs = None
+                        plan[currentYear]['yearSchedule'][quarter]['classes'].append({'name': cID, 'prereqs': prereqs, 'units': self.metadata[key][cID].getCredits()})
+                        self.completeClass(cID)
+                        classCount += 1
+                        balanceCount += 1
+                    # in original c++ code this next if statement is in the beginning for emphases
+                    if balanceCount >= 2:
+                        break
+            if DEBUG_MODE is True: print("Current plan (end of while):", plan)
+            if currentYear > 30:
+                if DEBUG_MODE is True: print("Cannot build plan. Exiting program.")
+                sys.exit(1)
+        if DEBUG_MODE is True: print("buildPlan(): Plan complete!")
+        return plan
+
+def buildFourYearPlan(majorMap, coreMap, prevCompletedClassesMap, creditsCompleted):
+    year = 2019
+    fourYearPlan = FourYearPlan(majorMap, coreMap, creditsCompleted)
+    for doneClass in prevCompletedClassesMap:
+        fourYearPlan.completeClass(doneClass)
+    return fourYearPlan.buildPlan(year)
 
 def getJson(jsonFileName):
     with open(jsonFileName, 'r') as data_f:
@@ -85,12 +226,6 @@ def getMysqlConn():
         MYSQLDB = 'PlanToGrad'
         MYSQLPW = None
 
-#       MYSQLHOST = 'remotemysql.com'
-#       MYSQLPORT = 3306
-#       MYSQLUSER = 'GYTE3BoCBP'
-#       MYSQLDB = 'GYTE3BoCBP'
-#       MYSQLPW = '27v0MR70aB'
-
         # MySQL connection
         print("Connecting to MySQL server...")
         conn = pymysql.connect( MYSQLHOST,
@@ -120,11 +255,6 @@ def survey():
     csciEmphases = getJson(csciEmphasesJson)
     csciMajorReqs = getJson(csciMajorReqsJson)
     coreReqs = getJson(coreReqsJson)
-
-#   Print to console log for debugging
-#   print(csciEmphases)
-#   print(csciMajorReqs)
-#   print(coreReqs)
 
     return render_template('survey.html', csciEmphases=csciEmphases, csciMajorReqs=csciMajorReqs, coreReqs=coreReqs)
 
@@ -183,86 +313,54 @@ def schedule():
 #   conn.commit()
 #   print("\nSQL commands committed to database")
 
-    # Write to algorithm input file
-    with open(classObjectsFile, 'w') as infile:
-        # Query major classes and write to file
-        queryMajorClasses = "SELECT a.CourseID, CourseName, QuarterOffered, CreditGiven FROM Classes AS a LEFT JOIN MajorReqs AS b ON a.CourseID = b.CourseID WHERE MajorName = \'" + majorAndEmphasis + "\';"
-        cur.execute(queryMajorClasses)
-        queriedMajorClasses = cur.fetchall()
-        for aClass in queriedMajorClasses:
-            classID = aClass[0]
-            className = aClass[1]
-            subject = majorAndEmphasis
-            quarterOffered = aClass[2]
-            creditGiven = aClass[3]
-            lineToAppend = classID + ";" + className + ";" + subject + ";" + quarterOffered + ";" + str(creditGiven) + ";"
-            queryPrereqs = "SELECT PreReqName from Prereqs where CourseID=\'" + classID + "\'"
-            cur.execute(queryPrereqs)
-            queriedPrereqs = cur.fetchall()
-            for prereq in queriedPrereqs:
-                lineToAppend = lineToAppend + prereq[0] + ";"
-            infile.write(lineToAppend + "\n")
-        infile.write("!\n")
-        # Query core classes and write to file
-        queryCoreClasses = "SELECT a.CourseID, CourseName, QuarterOffered, CreditGiven FROM Classes AS a LEFT JOIN MajorReqs AS b ON a.CourseID = b.CourseID WHERE MajorName = \'Core\';"
-        cur.execute(queryCoreClasses)
-        queriedCoreClasses = cur.fetchall()
-        for aClass in queriedCoreClasses:
-            classID = aClass[0]
-            className = aClass[1]
-            subject = majorAndEmphasis
-            quarterOffered = aClass[2]
-            creditGiven = aClass[3]
-            lineToAppend = classID + ";" + className + ";" + subject + ";" + quarterOffered + ";" + str(creditGiven) + ";"
-            queryPrereqs = "SELECT PreReqName from Prereqs where CourseID=\'" + classID + "\'"
-            cur.execute(queryPrereqs)
-            queriedPrereqs = cur.fetchall()
-            for prereq in queriedPrereqs:
-                lineToAppend = lineToAppend + prereq[0] + ";"
-            infile.write(lineToAppend + "\n")
-        infile.write("!\n")
-        # Write two random emphasis classes lol
-        infile.write("CSCI 168;Computer Graphics;" + majorAndEmphasis + ";F;5;CSCI 10;MATH 13;\nCOEN 166;Artificial Intelligence;" + majorAndEmphasis + ";FS;5;CSCI 61;MATH 51;\n")
-        infile.write("!\n")
-        # Classes previously taken
-        for classID in allClassesTaken:
-            lineToWrite = ""
-            queryTakenClass = "SELECT CourseName, QuarterOffered, CreditGiven from Classes where CourseID=\'" + classID + "\'"
-            cur.execute(queryTakenClass)
-            queriedTakenClass = cur.fetchall()
-            print(queriedTakenClass)
-            className = queriedTakenClass[0][0]
-            subject = majorAndEmphasis
-            quarterOffered = queriedTakenClass[0][1]
-            creditGiven = queriedTakenClass[0][2]
-            lineToAppend = classID + ";" + className + ";" + subject + ";" + quarterOffered + ";" + str(creditGiven) + ";"
-            infile.write(lineToAppend + "\n")
+    # Query major classes and write to file
+    majorMap = {}
+    coreMap = {}
+    doneClassesMap = {}
+    creditsCompleted = 0
+    queryMajorClasses = "SELECT a.CourseID, CourseName, QuarterOffered, CreditGiven FROM Classes AS a LEFT JOIN MajorReqs AS b ON a.CourseID = b.CourseID WHERE MajorName = \'" + majorAndEmphasis + "\';"
+    cur.execute(queryMajorClasses)
+    queriedMajorClasses = cur.fetchall()
 
-    # Compile C++ 4-year plan algorithm as subprocess
-    subprocess.check_call(
-        ('g++', '-o', 'classScheduler.out', algoClass, algoHashMap, algoPlan, algoStudent, algoDriver),
-        stdin=subprocess.DEVNULL)
+    # Get two random emphasis classes lol
+    queryTwoEmphasisClasses = "SELECT a.CourseID, CourseName, QuarterOffered, CreditGiven FROM Classes AS a LEFT JOIN MajorReqs AS b ON a.CourseID = b.CourseID WHERE a.CourseID = \'CSCI 168\' OR a.CourseID = \'COEN 166 and L\';"
+    cur.execute(queryTwoEmphasisClasses)
+    queriedMajorClasses += cur.fetchall()
 
-    # Run executable
-    with open(fourYearPlanJson, 'w') as outfile:
-        subprocess.check_call(
-            ('./classScheduler.out', classObjectsFile),
-#           stdin = None,
-            stdout = outfile,
-            universal_newlines = True)
+    for aClass in queriedMajorClasses:
+        classID = aClass[0]
+        creditGiven = aClass[3]
+        classObj = scuClass(classID, aClass[1], majorAndEmphasis, aClass[2], creditGiven)
+        queryPrereqs = "SELECT PreReqName from Prereqs where CourseID=\'" + classID + "\'"
+        cur.execute(queryPrereqs)
+        queriedPrereqs = cur.fetchall()
+        for prereq in queriedPrereqs:
+            classObj.pushPreReq(prereq[0])
+        majorMap[classID] = classObj
+        if classID in allClassesTaken:
+            doneClassesMap[classID] = classObj
+            creditsCompleted += creditGiven
 
-    # Delete all the bullshit outputted by the executable before the JSON
-    lines = open(fourYearPlanJson, 'r').readlines()
-    search = "["
-    for jsonStart, line in enumerate(lines):
-        if search in line:
-            break
-    if jsonStart < len(lines) - 1:
-        with open(fourYearPlanJson, 'w') as outfile:
-            outfile.write(''.join(lines[jsonStart:]))
+    # Query core classes and write to file
+    queryCoreClasses = "SELECT a.CourseID, CourseName, QuarterOffered, CreditGiven FROM Classes AS a LEFT JOIN MajorReqs AS b ON a.CourseID = b.CourseID WHERE MajorName = \'Core\';"
+    cur.execute(queryCoreClasses)
+    queriedCoreClasses = cur.fetchall()
 
-    # Store four year plan to Python dict from JSON
-    fourYearPlan = getJson(fourYearPlanJson)
+    for aClass in queriedCoreClasses:
+        classID = aClass[0]
+        creditGiven = aClass[3]
+        classObj = scuClass(classID, aClass[1], "Core", aClass[2], creditGiven)
+        queryPrereqs = "SELECT PreReqName from Prereqs where CourseID=\'" + classID + "\'"
+        cur.execute(queryPrereqs)
+        queriedPrereqs = cur.fetchall()
+        for prereq in queriedPrereqs:
+            classObj.pushPreReq(prereq[0])
+        coreMap[classID] = classObj
+        if classID in allClassesTaken:
+            doneClassesMap[classID] = classObj
+            creditsCompleted += creditGiven
+
+    fourYearPlan = buildFourYearPlan(majorMap, coreMap, doneClassesMap, creditsCompleted)
 
     # Close connection to database
     cur.close()
