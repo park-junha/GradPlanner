040f38ae495abcf83ab344a2372167bbd7676948
park-junha <jpark3@scu.edu>
2020-03-08 00:01:37 -0800

    awslogin.sh now starts within aws folder of repository upon execution

--- aws/awslogin.sh
+++ aws/awslogin.sh
@@ -1,1 +1,1 @@
-ssh -i ~/Desktop/gradplanner10.pem ubuntu@ec2-13-52-238-219.us-west-1.compute.amazonaws.com
+ssh -i ~/Desktop/gradplanner10.pem -t ubuntu@ec2-13-52-238-219.us-west-1.compute.amazonaws.com "cd GradPlanner/aws; /bin/bash -i"
